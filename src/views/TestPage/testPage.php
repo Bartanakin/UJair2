efa61036661b15c10162d1e9feecd1707423bdff
Bartanakin <bartanakin@interia.pl>
2022-03-31 22:20:17 +0200

    visualizing test

--- src/views/TestPage/testPage.php
+++ src/views/TestPage/testPage.php
@@ -1,4 +1,11 @@
 <?="Hello world but without Putin!";?>
 <pre>
-    <?php var_dump($this -> params); ?>
+    <table>
+        <?php foreach( $this -> params as $record): ?>
+        <tr>
+            <td><?= $record["ID"] ?></td>
+            <td><?= $record["value1"] ?></td>
+        </tr>
+        <?php endforeach; ?>
+    </table>
 </pre>
