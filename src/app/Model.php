c0f67ca1fb19d342420cefb1cb7d575385a8301e
ThisNameIsNotAvailable <aliaxeyka@gmail.com>
2022-04-25 23:24:28 +0200

    add constant

--- src/app/Model.php
+++ src/app/Model.php
@@ -5,6 +5,7 @@ namespace App;
 abstract class Model
 {
     protected $dbConnection;
+    static public string $dateFormat = 'Y-m-d H:i:s';
     protected function __construct()
     {
         $this -> dbConnection = App::getDataBaseConnection() -> getPDO();
