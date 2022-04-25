bcbe814aedca6cd47222525306cadec18a833c93
ThisNameIsNotAvailable <aliaxeyka@gmail.com>
2022-04-25 18:17:27 +0200

    fix

--- src/SQL/Views/reservedSeatsForFlight.sql
+++ src/SQL/Views/reservedSeatsForFlight.sql
@@ -2,4 +2,6 @@
 CREATE OR REPLACE VIEW ReservedSeatsForFlight
 AS
 SELECT F.ID, T.NumberOfSeat, AT.Max_number_of_passengers FROM Tickets AS T
-                                                                  JOIN Flights AS F ON F.ID = T.FlightID;
+                                      JOIN Flights AS F ON F.ID = T.FlightID
+                                      JOIN Airplanes AS A ON F.AirPlaneID = A.ID
+                                      JOIN AirplaneTypes AS AT ON A.AirplaneTypeID = AT.ID; 
