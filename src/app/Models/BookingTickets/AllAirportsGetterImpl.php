079204c89c0303163fd843779686a9de07cacdac
Bartanakin <bartanakin@interia.pl>
2022-04-25 20:42:07 +0200

    repaired container

--- src/app/Models/BookingTickets/AllAirportsGetterImpl.php
+++ src/app/Models/BookingTickets/AllAirportsGetterImpl.php
@@ -5,8 +5,8 @@ namespace App\Models\BookingTickets;
 use App\Entities\Airport;
 
 class AllAirportsGetterImpl extends \App\Model implements \App\Interfaces\BookingTicketsInterfaces\AllAirportsGetter {
-    protected array $airports = [];
-    public function __construct(){
+
+    public function __construct(protected array $airports = []){
         parent::__construct();
     }
     function run(): array {
@@ -14,8 +14,7 @@ class AllAirportsGetterImpl extends \App\Model implements \App\Interfaces\Bookin
         $statement = $this -> dbConnection -> prepare($query);
         $statement -> execute();
         while($data = $statement -> fetch()) {
-            $this->airports[] = new Airport($data['ID'], $data['Airport_name']);
-//            echo $data['ID'] . " " . $data['Airport_name'] . '<br/ >';
+            $this -> airports[] = new Airport($data['ID'], $data['Airport_name']);
         }
         return $this -> airports;
     }
