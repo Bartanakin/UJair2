e658bfdd44f90112df542b9568675239b590f261
Bartanakin <bartanakin@interia.pl>
2022-05-13 12:41:08 +0200

    small changes

--- src/views/PlannerAppPages/flight_editor.php
+++ src/views/PlannerAppPages/flight_editor.php
@@ -1,2 +1,17 @@
-<?php
-echo "Flight editor page :)";
+<head>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Koulen&display=swap" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="commonStyle" >
+    <link rel="stylesheet" type="text/css" href="flightEditorStyle" >
+</head>
+<body>
+    <header>
+        <div class="headerTitle">
+            UJAIR2 - Flight editor
+        </div>
+    </header>
+    <div class="wrapper">
+
+    </div>
+</body>
