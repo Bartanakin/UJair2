475a324eede6d81392791a1ab7e167028041aeb3
ThisNameIsNotAvailable <aliaxeyka@gmail.com>
2022-05-28 01:05:22 +0200

    add pods

--- PassengerApp/buyTickets/buyTickets/Controllers/BookingTicketsViewController.swift
+++ PassengerApp/buyTickets/buyTickets/Controllers/BookingTicketsViewController.swift
@@ -6,12 +6,12 @@
 //
 
 import UIKit
+import SearchTextField
 
 class BookingTicketsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
     }
     
