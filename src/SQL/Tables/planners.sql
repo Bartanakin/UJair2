20949d9eb1f08e3abecb12122606f7d60229fb96
ThisNameIsNotAvailable <aliaxeyka@gmail.com>
2022-06-02 18:30:31 +0200

    server test

--- src/SQL/Tables/planners.sql
+++ src/SQL/Tables/planners.sql
@@ -1,3 +1,4 @@
+
 CREATE TABLE Planners
 (
     ID int auto_increment PRIMARY KEY,
@@ -6,4 +7,4 @@ CREATE TABLE Planners
 );
 
 INSERT INTO Planners VALUES
-( NULL, 'planner1','$2y$10$z4UyLSm4VrUQT8Hdj4Bgjuo3wmcIAzd7w3rqvrN2ITtcbbx1Avv1q');
+( NULL, 'planner1','$2y$10$7y6Q38rb2w.kLDjswxa3BuRI2yXheCYnfxh0njX./4iP2KTpCL9vW');
